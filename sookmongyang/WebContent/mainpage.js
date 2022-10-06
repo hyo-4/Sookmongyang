74994c69ff7b8ff06bbac2e9bc1cf38ce44deb8d
hyo-4 <shyo0218@naver.com>
2022-10-06 16:26:58 +0900

    init commit

---
+++ sookmongyang/WebContent/mainpage.js
@@ -0,0 +1,41 @@
+
+let curPos = 0;
+let postion = 0;
+
+let cursor; // 커서 이미지를 가진 <div> 객체
+function makeCursor() {
+	cursor = document.getElementById("cursor");
+	document.onmousemove = function (e) {
+		cursor.style.left = e.pageX + "px";
+		cursor.style.top =e.pageY+7+"px";
+		
+		
+	}
+}
+
+   var counter = 1;
+    setInterval(function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 4){
+        counter = 1;
+      }
+    }, 2000);
+    
+    var ret = null; 
+window.onclick = function(e)
+{
+    if(e.target.tagName.toLowerCase() != "li")
+    	return;
+    else{
+		e.target.style.fontSize = "1.2em";
+		e.target.style.color = "#63cfa7";
+		}
+    
+    if(ret != null)
+    {
+        ret.style.fontSize = "1em";
+		ret.style.color = "black";
+    }
+    ret = e.target;
+}
